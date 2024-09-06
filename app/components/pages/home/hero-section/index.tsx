14d206e90f4cf00a5e1b092115089b0104eebef6
Gilvan Neto <gilvas@email.com>
2024-09-06 15:43:46 -0300

    update: image profile

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,38 +1,42 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import { TechBadge } from "../../../tech-badge"
-import { Button } from "@/app/components/button"
-import { HiArrowNarrowRight } from 'react-icons/hi'
-import { TbBrandGithub, TbBrandLinkedin, TbBrandWhatsapp } from 'react-icons/tb'
-import { motion } from "framer-motion"
-import { techBadgeAnimation } from "@/app/lib/animation"
+import Image from "next/image";
+import { TechBadge } from "../../../tech-badge";
+import { Button } from "@/app/components/button";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import {
+  TbBrandGithub,
+  TbBrandLinkedin,
+  TbBrandWhatsapp,
+} from "react-icons/tb";
+import { motion } from "framer-motion";
+import { techBadgeAnimation } from "@/app/lib/animation";
 
 const MOCK_CONTACTS = [
   {
-    url: 'http://github.com.br/brcneto',
-    icon: <TbBrandGithub />
+    url: "http://github.com.br/brcneto",
+    icon: <TbBrandGithub />,
   },
   {
-    url: 'https://linkedin.com/in/gilvan-neto-desenvolvedor/',
-    icon: <TbBrandLinkedin />
+    url: "https://linkedin.com/in/gilvan-neto-desenvolvedor/",
+    icon: <TbBrandLinkedin />,
   },
   {
-    url: 'https://api.whatsapp.com/send?phone=+5481991881256&text=Ol%C3%A1%21+Gostaria+de+entrar+em+contato+para+falar+sobre+uma+proposta%21%21%21',
-    icon: <TbBrandWhatsapp />
+    url: "https://api.whatsapp.com/send?phone=+5481991881256&text=Ol%C3%A1%21+Gostaria+de+entrar+em+contato+para+falar+sobre+uma+proposta%21%21%21",
+    icon: <TbBrandWhatsapp />,
   },
-]
+];
 
-const techs = ['TypeScript', 'JavaScript', 'NextJS', 'ReactJS', 'TailwindCSS']
+const techs = ["TypeScript", "JavaScript", "NextJS", "ReactJS", "TailwindCSS"];
 
 export const HeroSection = () => {
   const handleContact = () => {
-    const contactSection = document.querySelector('#contact')
+    const contactSection = document.querySelector("#contact");
 
     if (contactSection) {
-      contactSection.scrollIntoView({ behavior: 'smooth' })
+      contactSection.scrollIntoView({ behavior: "smooth" });
     }
-  }
+  };
 
   return (
     <section className="w-full h-[650px] 2xl:h-[755px]  bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-center pb-10 py-36 lg:pb-[110px]">
@@ -48,9 +52,10 @@ export const HeroSection = () => {
           <h2 className="text-4xl font-medium mt-2">Gilvan Neto</h2>
 
           <p className=" text-gray-400 my-6 text-sm sm:text-base">
-            Sou desenvolvedor de software voltado para a web, desenvolvo Frontend de aplicações
-            utilizando as principais tecnologias do mercado, ampliando meu conhecimento e buscando
-            me manter sempre atualizado. Estou muito empolgado para trabalhar com você!
+            Sou desenvolvedor de software voltado para a web, desenvolvo
+            Frontend de aplicações utilizando as principais tecnologias do
+            mercado, ampliando meu conhecimento e buscando me manter sempre
+            atualizado. Estou muito empolgado para trabalhar com você!
           </p>
 
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
@@ -59,7 +64,8 @@ export const HeroSection = () => {
                 name={tech}
                 key={index}
                 {...techBadgeAnimation}
-                transition={{ duration: 0.2, delay: index * 0.1 }} />
+                transition={{ duration: 0.2, delay: index * 0.1 }}
+              />
             ))}
           </div>
 
@@ -82,8 +88,6 @@ export const HeroSection = () => {
               ))}
             </div>
           </div>
-
-
         </motion.div>
 
         <motion.div
@@ -95,12 +99,12 @@ export const HeroSection = () => {
           <Image
             width={420}
             height={404}
-            src="/images/profile-gilvas.jpeg"
+            src="/images/profile-gilvas.jpg"
             alt="Foto de perfil"
             className="w-[300px] h-[300px] lg:w-[375px] 2xl:w-[404px] lg:h-[375px] 2xl:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-full object-cover"
           />
         </motion.div>
       </div>
-    </section >
-  )
-}
+    </section>
+  );
+};
